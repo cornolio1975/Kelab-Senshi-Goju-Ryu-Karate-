3c4a7ad2cb2b60760e8de226d4f36dc78acdfead
cornolio1975 <108326822+cornolio1975@users.noreply.github.com>
2026-07-16 23:15:18 +0000

    Deploying to gh-pages from @ cornolio1975/Kelab-Senshi-Goju-Ryu-Karate-@c5ae8198938c37483bf0e9c2da89bb566ec82271 🚀

---
+++ _next/static/6K6bQ-Jc41AGM8H8Mf358/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Kelab-Senshi-Goju-Ryu-Karate-//_next/:path+",
+        "destination": "/Kelab-Senshi-Goju-Ryu-Karate-/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
